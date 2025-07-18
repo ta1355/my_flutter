c9c1bc18f8dbdfee973d5980e102104827d73005
ta1355 <eorbs255@naver.com>
2025-07-19 00:14:43 +0900

    버튼 제작 연습

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ void main() {
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,6 +16,7 @@ class App extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: 80),
               Row(
@@ -35,6 +38,41 @@ class App extends StatelessWidget {
                         style: TextStyle(color: Colors.white.withOpacity(0.5)),
                       ),
                     ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 120),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text('Transfer', style: TextStyle(fontSize: 22)),
+                    ),
                   ),
                 ],
               ),
